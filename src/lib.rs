ce70006864ec969cb9f0e2ae4933c3b86cc36c6f
amozalev <mozalev.alexey@gmail.com>
2025-02-22 18:53:15 +0200

    Replaced Neighbor struct by Link one.

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,22 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
-pub enum Border {
+pub enum BorderSide {
     North = 0b0001,
     East = 0b0010,
     South = 0b0100,
     West = 0b1000,
 }
 
+struct Link {
+    border: usize,
+    neighbor_ind: usize,
+    neighbor_border: usize,
+}
+
 #[derive(Debug)]
 #[wasm_bindgen]
 pub struct Cell(usize);
-
-struct Neighbor(usize, usize, usize);
 
 #[wasm_bindgen]
 pub struct Maze {
@@ -31,7 +35,7 @@ impl Maze {
         let size = width * width;
         let mut grid: Vec<Cell> = vec![];
 
-        for i in 0..size {
+        for _ in 0..size {
             grid.push(Cell(0b0000));
         }
 
@@ -42,38 +46,34 @@ impl Maze {
 
     fn generate_maze(&mut self, width: usize) {
         for i in 0..self.size {
-            let mut neighbors: Vec<Neighbor> = vec![];
+            let mut neighbors: Vec<Link> = vec![];
 
             let neighbor_ind = i - width;
             if i >= width && neighbor_ind < self.size {
-                neighbors.push(Neighbor(
-                    Border::North as usize,
+                neighbors.push(Link {
+                    border: BorderSide::North as usize,
                     neighbor_ind,
-                    Border::South as usize,
-                ));
+                    neighbor_border: BorderSide::South as usize,
+                });
             }
 
             let row = i / width;
             let neighbor_ind = i + 1;
             let neighbor_row = neighbor_ind / width;
             if neighbor_ind > 0 && neighbor_ind < self.size && row == neighbor_row {
-                neighbors.push(Neighbor(
-                    Border::East as usize,
+                neighbors.push(Link {
+                    border: BorderSide::East as usize,
                     neighbor_ind,
-                    Border::West as usize,
-                ));
+                    neighbor_border: BorderSide::West as usize,
+                });
             }
 
             let num = rand::thread_rng().gen_range(0..neighbors.len().max(1));
 
             if neighbors.get(num).is_some() {
-                let neighbor = &neighbors[num];
-                let border = neighbor.0;
-                let neighbor_ind = neighbor.1;
-                let neighbor_border = neighbor.2;
-
-                self.grid[i] = Cell(self.grid[i].0 | border);
-                self.grid[neighbor_ind] = Cell(self.grid[neighbor_ind].0 | neighbor_border);
+                let link = &neighbors[num];
+                self.grid[i].0 |= link.border;
+                self.grid[link.neighbor_ind].0 |= link.neighbor_border;
             }
         }
     }
